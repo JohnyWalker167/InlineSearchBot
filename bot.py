546947bda3452d56d713963f51c3fb32745b51c2
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2025-08-25 20:03:04 +0000

    minor change

--- bot.py
+++ bot.py
@@ -569,6 +569,15 @@ async def inline_query_handler(client, inline_query):
         )
         return
     
+    if not query:
+        await inline_query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text="Enter Valid Movie/Series Click here to know more.",
+            switch_pm_parameter="help"
+        )
+        return
+    
     try:
         offset = int(inline_query.offset) if inline_query.offset else 0
     except Exception:
@@ -603,35 +612,15 @@ async def inline_query_handler(client, inline_query):
     # Set next_offset if more results are available
     next_offset = str(offset + SEARCH_PAGE_SIZE) if (offset + SEARCH_PAGE_SIZE) < total_count else ""
 
-    if results:
-        await inline_query.answer(
-            results,
-            cache_time=0,
-            next_offset=next_offset,
-            switch_pm_text=f"Result for {query}",
-            switch_pm_parameter="start"
-        )
-        return
+    await inline_query.answer(
+        results,
+        cache_time=0,
+        next_offset=next_offset,
+        switch_pm_text=f"Result for {query}" if results else "Enter Valid Movie/Series Click here to know more.",
+        switch_pm_parameter="start" if results else "help"
+    )
+    return
 
-    if not results:
-        await inline_query.answer(
-            results=[],
-            cache_time=0,
-            switch_pm_text="Enter Valid Movie/Series Click here to know more.",
-            switch_pm_parameter="help"
-
-        )
-        return
-    
-    if not query:
-        await inline_query.answer(
-            results=[],
-            cache_time=0,
-            switch_pm_text="Enter Valid Movie/Series Click here to know more.",
-            switch_pm_parameter="help"
-        )
-        return
-        
 @bot.on_message(filters.via_bot)
 async def private_file_handler(client, message: Message):
         bot.loop.create_task(delete_after_delay(message))
